a6bc853fd48dd45141a3e6efb95160476ce95236
Iain Evans <iaine@protonmail.com>
2023-06-26 21:36:23 +0100

    Updated Structs Examples

--- Projects/Structs/main.go
+++ Projects/Structs/main.go
@@ -2,17 +2,36 @@ package main
 
 import "fmt"
 
+type contactinfo struct {
+	email    string
+	postCode string
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactinfo // Can add structs in structs
 }
 
 func main() {
-	bob := person{"Bob", "Bobson"} // Not recommended
-	dug := person{firstName: "Dug", lastName: "Stanhope"}
+	// bob := person{"Bob", "Bobson"}                        // Option #1 - Not recommended
+	// dug := person{firstName: "Dug", lastName: "Stanhope"} // Option #2
 
-	fmt.Println(bob)
-	fmt.Println(dug)
-	fmt.Println(dug.firstName)
+	// bob.firstName = "bobby" // Update
+
+	// fmt.Println(bob)
+	// fmt.Println(dug)
+	// fmt.Println(dug.firstName)
+	// fmt.Printf("%+v", dug) // Print types
+
+	timmy := person{
+		firstName: "Timmy",
+		lastName:  "Tim",
+		contact: contactinfo{
+			email:    "jim@southpark.com",
+			postCode: "ST16 2MY",
+		},
+	}
+	fmt.Printf("%+v", timmy)
 
 }
